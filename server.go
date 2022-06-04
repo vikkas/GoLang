0fb91290da54dd5d3f51561e9f5a38006e6ca048
Vikas Jindal <vikkasjindal@gmail.com>
2022-06-04 11:07:04 +0530

    Add files via upload

---
+++ server.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"server/service"
+
+	"github.com/gin-gonic/gin"
+)
+
+var (
+	videoService    service.VideoService       = service.New()
+	videoController controller.VideoController = controller.New(videoService)
+)
+
+func main() {
+	server := gin.Default()
+
+	server.GET("/videos", func(ctx *gin.Context) {
+		ctx.JSON(200, videoController.FinaAll())
+	})
+	server.POST("/videos", func(ctx *gin.Context) {
+		ctx.JSON(200, videoController.Save(ctx))
+	})
+	server.Run(":8080")
+}
